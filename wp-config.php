11bcebbdbf7aafb6601c0a4c3e694884ed02afc9
unknown <asadwmr@gmail.com>
2022-03-04 01:17:47 +0500

    one

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define( 'DB_NAME', 'clickmarket' );
+define( 'DB_NAME', 'testmarket' );
 
 /** Имя пользователя MySQL */
 define( 'DB_USER', 'root' );
